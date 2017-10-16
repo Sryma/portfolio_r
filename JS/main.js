7d82b13431bf0267c7bdc56a3e794993ce867019
Sryma <rurahgst@gmail.com>
2017-10-15 21:20:26 -0500

    first

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,10 @@
 
-// var typed = new Typed(".element", {
-//   strings: ["This is a JavaScript library", "This is an ES6 module"],
-//   smartBackspace: true // Default value
-// });
-// var typed6 = new Typed('.element', {
-//     //strings: ['I am front end web developer^1000\n `I am good at` ^1000\n `HTML ^1000\n `CSS3 ^1000\n` JavaScript ^1000\n`Jquery ^1000\n'],
-    
-//     typeSpeed: 40,
-//     backSpeed: 0,
-//     loop: true
-//   });
-// 
+
 var typed3 = new Typed('.element', {
     strings: ['Hi I am: <i style="color:maroon">web developer</i>', 'I love to: <strong style="color:maroon">make website</strong>', 'Welcome to my <i style="color:maroon">portfolio</i>'],
     typeSpeed: 0,
     backSpeed: 0,
-    // smartBackspace: false, // this is a default
+    smartBackspace: false, // this is a default
     loop: true
   });
 var typed4 = new Typed('.element_1', {
